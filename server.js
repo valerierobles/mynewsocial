ee6fe3aa59593d6688c668e43387feeab2613d1b
valerierobles <roblesv08@gmail.com>
2023-05-12 21:58:41 -0500

    server.js based off student activity

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
